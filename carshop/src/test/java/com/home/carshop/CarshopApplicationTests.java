b7dfb74a306d8aa3f36a10b052f8b666a483e150
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-03-07 23:20:45 +0900

    학원시험연습 - 파일 경로 수정으로 인해 carshopfront 새로 생성, 자동차 쇼핑몰 landing페이지 만드는 중

---
+++ carshop/src/test/java/com/home/carshop/CarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
