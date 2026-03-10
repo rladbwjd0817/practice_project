a509cc94a8b78daa715e319d909ba594cba16767
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-03-10 23:43:15 +0900

    구매자등록 페이지 그림complete

--- carshop/src/main/java/com/home/carshopfront_practice/src/pages/manage/SalesRegister.jsx
+++ carshop/src/main/java/com/home/carshopfront_practice/src/pages/manage/SalesRegister.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SalesRegister.module.css'
 import Select from '../../components/common/Select'
 import Input from '../../components/common/Input'
@@ -7,52 +7,80 @@ import Button from '../../components/common/Button'
 // 구매자 등록 페이지 //
 
 const SalesRegister = () => {
+  /* 입력한 구매자 정보 저장할 state 변수 */
+  const [Regsales, setRegSales] = useState({})
 
+  /* 조회한 차량 모델, 색상 데이터 저장할 state 변수 */
+  const [modelAndColor, setModelAndColor] = useState([])
 
   return (
     <div className={styles.container}>
       <h2>Sales Ragister</h2>
-      <div>
-        <div className={styles.regForm_title_div}>
-          <div>Register New Car Model</div>
+        <div>
+          <div className={styles.salesRegForm_letter_div}>Sales Ragister</div>
           <div className={styles.line_div}></div>
-          <div className={styles.manufacturer_div}>
-            <p>Manufacturer</p>
-            {/* select 컴포넌트 */}
-            <Select value='1'>
-              {/* 일단 출력해보고 수정하기 -> value에 regCar에 저장된 제조사 값 꺼내서 넣기'{regCar.manu--}' */}
-              {
-                // regCar.map((car, index) => {
-                //   return(
-                //     <>
-                //       <option key={index} value="1">선택</option>
-                //       <option value={car.manufacturer}>기아</option>
-                //       <option value={car.manufacturer}>현대</option>
-                //       <option value={car.manufacturer}>쉐보레</option>
-                //       <option value={car.manufacturer}>Benz</option>
-                //       <option value={car.manufacturer}>BMW</option>
-                //       <option value={car.manufacturer}>Audi</option>
-                //       <option value={car.manufacturer}>Volvo</option>
-                //     </>
-                //   )
-                // })
-              }
-              
-            </Select>
-          </div>
-          <div className={styles.carName_div}>
-            <p>Car Name</p>
+          <div className={styles.salesName_div}>
+            <p>구매자명</p>
             <Input 
-              size='long'
-              placeholder= '모델명을 입력하세요.'
+              size='medium'
+              value={Regsales.name}
+              onChange={e => {}}
             />
           </div>
-          <div className={styles.price_div}>
-            <p>Price</p>
+          <div className={styles.select_div}>
+            <div className={styles.carColor_div}>
+              <p>색상</p>
+              {/* 조회한 색상 데이터 option에 넣어야 함! */}
+              <Select value='1' className={styles.carColor_select}>
+                
+                {
+                  modelAndColor.map((color, i) => {
+                    return(
+                      <>
+                        <option     
+                          value="1"
+                          key={i}
+                        >선택</option>
+                        <option value="">{color}</option>
+                        <option value="">{color}</option>
+                        <option value="">{color}</option>
+                      </>
+                    )
+                  })
+                }
+              </Select>
+            </div>
+            <div className={styles.carModel_div}>
+              <p>모델</p>
+              {/* 조회한 모델 데이터 option에 넣어야 함! */}
+              <Select value='' className={styles.carModel_select}>
+                {
+                  modelAndColor.map((model, i) => {
+                    return(
+                      <>
+                        <option 
+                          value=""
+                          key={i}
+                        >선택</option>
+                        <option value="">{model}</option>
+                        <option value="">{model}</option>
+                        <option value="">{model}</option>
+                        <option value="">{model}</option>
+                        <option value="">{model}</option>
+                        <option value="">{model}</option>
+                      </>
+                    )
+                  })
+                }
+              </Select>
+            </div>
+          </div>
+          <div className={styles.salesRegTel_div}>
+            <p>연락처</p>
             <Input 
-              type='number'
-              size='long'
-              placeholder= '가격을 입력하세요.'
+              size='medium'
+              value={Regsales.tel}
+              onChange={e => {}}
             />
           </div>
           <div className={styles.regBtn_div}>
@@ -60,10 +88,10 @@ const SalesRegister = () => {
               title='Register'
               variant='blue'
               size='small'
+              onClick={e => {}}
             />
           </div>
         </div>
-      </div>
     </div>
   )
 }
