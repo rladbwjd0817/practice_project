bab51eaf293ba2e0839e714f47499b50d202dc3a
rladbwjd0817 <rladbwjd0817@gmail.com>
2026-03-21 23:46:56 +0900

    join pront complete

--- carshop/src/main/carshopfront_practice/src/pages/member/Join.jsx
+++ carshop/src/main/carshopfront_practice/src/pages/member/Join.jsx
@@ -3,55 +3,96 @@ import styles from './Join.module.css'
 import Input from '../../components/common/Input'
 import Button from '../../components/common/Button'
 import Select from '../../components/common/Select'
+import { PiWarningCircleLight } from "react-icons/pi";
+import { HiOutlineChevronLeft } from "react-icons/hi2";
+import { Link } from 'react-router-dom'
 
 
 const Join = () => {
   return (
     <div className={styles.container}>
       <div className={styles.join_div}>
-        <div>
+        <div className={styles.join_title}>
           {/* 타이틀 */}
           <h2>Velora 계정 정보 입력</h2>
         </div>
 
-       <div>
-          <Input placeholder='이메일'/> {/* 이메일 */}
+       <div className={styles.join_input_div}>
           <div>
+            <span className={styles.asterisk}>*</span>
+            <Input placeholder='이메일'/>
+          </div>
+
+          <div className={styles.tel_div}>
             {/* 전화번호 */}
+            <span className={styles.asterisk}>*</span>
             <Select />
+            <span className={styles.asterisk}>*</span>
             <Input type='number' placeholder='전화번호'/>
             <Button title='인증요청'/>
           </div> 
-          <Input type='password' placeholder='비밀번호'/> {/* 비밀번호 */}
-          <Input type='password' placeholder='비밀번호 확인'/> {/* 비밀번호 확인 */}
+
+          <div>
+            <span className={styles.asterisk}>*</span>
+            <Input type='password' placeholder='비밀번호'/>
+          </div> 
+
+          <div>
+            <span className={styles.asterisk}>*</span>
+            <Input type='password' placeholder='비밀번호 확인'/>
+          </div> 
        </div>
 
-       <div>
-        {/* 이름 */}
+       <div className={styles.name_div}>
         <p>이름</p>
-        <Input />
-        <Input />
+        <span className={styles.asterisk}>*</span>
+        <Input className={styles.name_input}placeholder='성'/>
+        <span className={styles.asterisk}>*</span>
+        <Input placeholder='이름'/>
        </div>
 
-       <div>
+       <div className={styles.birth_div}>
         {/* 생년월일 */}
-        <Input type='date'/>
+        <span className={styles.asterisk}>*</span>
+        <p>생년월일</p>
+        <Input 
+          type='date'
+          className={styles.birth_input}
+        />
        </div>
 
-       <div>
+       <div className={styles.gender_div}>
         {/* 성별 */}
-        <Input type='radio' title='남성'/>
-        <Input type='radio' title='여성'/>
+        <span className={styles.asterisk}>*</span>
+        <p>성별</p>
+        <div>
+          <input 
+            type='radio' 
+            name='gender'
+          /> 남성
+          <input 
+            type='radio'
+            name='gender'
+          /> 여성
+        </div>
        </div>
 
-       <div>
-        <p>Velora 계정은 서비스 제공을 위해 최소한의 개인정보를 수집 및 이용합니다.</p>
+       <div className={styles.notice_div}>
+        <PiWarningCircleLight style={{marginTop : '2'}}/>
+        <p style={{whiteSpace: 'pre-line'}}>
+          {`Velora 계정은 서비스 제공을 위해 최소한의 개인정보를 수집 및 이용합니다.`}
+        </p>
+       </div>
+
+       <div className={styles.btn}>
         <Button title='확인'/>
        </div>
 
-       <div>
-        {/* 로그인 화면으로 돌아가기 */}
-        <p>로그인 화면으로 돌아가기</p>
+       <div className={styles.back_login}>
+        <HiOutlineChevronLeft className={styles.back_login_icon}/>
+        <Link to={'/velora-login'}>
+          <p style={{color : 'black', fontSize : '0.8rem'}}>로그인 화면으로 돌아가기</p>
+        </Link>
        </div>
 
       </div>
